1ce1b67f8472e765922df183da12287e0cc52e18
Olmo <prieto.sanchez.olmo@gmail.com>
2020-10-11 14:14:06 +0200

    Added mat4 inversion code

--- Project/include/maths.h
+++ Project/include/maths.h
@@ -1,6 +1,6 @@
-#ifndef __MATH_H__
-#define __MATH_H__
+#pragma once
 
+#include <cassert>
 #include <cstdint>
 #include <cstring>
 
@@ -220,6 +220,139 @@ struct Mat4
     memset(matrix, 0, sizeof(float) * 16);
   }
 
+  void inverse()
+  {
+    float inv[16];
+    float det = 0.0f;
+
+    inv[0] = matrix[5] * matrix[10] * matrix[15] -
+      matrix[5] * matrix[11] * matrix[14] -
+      matrix[9] * matrix[6] * matrix[15] +
+      matrix[9] * matrix[7] * matrix[14] +
+      matrix[13] * matrix[6] * matrix[11] -
+      matrix[13] * matrix[7] * matrix[10];
+
+    inv[4] = -matrix[4] * matrix[10] * matrix[15] +
+      matrix[4] * matrix[11] * matrix[14] +
+      matrix[8] * matrix[6] * matrix[15] -
+      matrix[8] * matrix[7] * matrix[14] -
+      matrix[12] * matrix[6] * matrix[11] +
+      matrix[12] * matrix[7] * matrix[10];
+
+    inv[8] = matrix[4] * matrix[9] * matrix[15] -
+      matrix[4] * matrix[11] * matrix[13] -
+      matrix[8] * matrix[5] * matrix[15] +
+      matrix[8] * matrix[7] * matrix[13] +
+      matrix[12] * matrix[5] * matrix[11] -
+      matrix[12] * matrix[7] * matrix[9];
+
+    inv[12] = -matrix[4] * matrix[9] * matrix[14] +
+      matrix[4] * matrix[10] * matrix[13] +
+      matrix[8] * matrix[5] * matrix[14] -
+      matrix[8] * matrix[6] * matrix[13] -
+      matrix[12] * matrix[5] * matrix[10] +
+      matrix[12] * matrix[6] * matrix[9];
+
+
+    det = matrix[0] * inv[0] + matrix[1] * inv[4] + matrix[2] * inv[8] + matrix[3] * inv[12];
+
+    if (det == 0.0f)
+    {
+      assert(false && "det == 0");
+      return;
+    }
+
+
+    inv[1] = -matrix[1] * matrix[10] * matrix[15] +
+      matrix[1] * matrix[11] * matrix[14] +
+      matrix[9] * matrix[2] * matrix[15] -
+      matrix[9] * matrix[3] * matrix[14] -
+      matrix[13] * matrix[2] * matrix[11] +
+      matrix[13] * matrix[3] * matrix[10];
+
+    inv[5] = matrix[0] * matrix[10] * matrix[15] -
+      matrix[0] * matrix[11] * matrix[14] -
+      matrix[8] * matrix[2] * matrix[15] +
+      matrix[8] * matrix[3] * matrix[14] +
+      matrix[12] * matrix[2] * matrix[11] -
+      matrix[12] * matrix[3] * matrix[10];
+
+    inv[9] = -matrix[0] * matrix[9] * matrix[15] +
+      matrix[0] * matrix[11] * matrix[13] +
+      matrix[8] * matrix[1] * matrix[15] -
+      matrix[8] * matrix[3] * matrix[13] -
+      matrix[12] * matrix[1] * matrix[11] +
+      matrix[12] * matrix[3] * matrix[9];
+
+    inv[13] = matrix[0] * matrix[9] * matrix[14] -
+      matrix[0] * matrix[10] * matrix[13] -
+      matrix[8] * matrix[1] * matrix[14] +
+      matrix[8] * matrix[2] * matrix[13] +
+      matrix[12] * matrix[1] * matrix[10] -
+      matrix[12] * matrix[2] * matrix[9];
+
+    inv[2] = matrix[1] * matrix[6] * matrix[15] -
+      matrix[1] * matrix[7] * matrix[14] -
+      matrix[5] * matrix[2] * matrix[15] +
+      matrix[5] * matrix[3] * matrix[14] +
+      matrix[13] * matrix[2] * matrix[7] -
+      matrix[13] * matrix[3] * matrix[6];
+
+    inv[6] = -matrix[0] * matrix[6] * matrix[15] +
+      matrix[0] * matrix[7] * matrix[14] +
+      matrix[4] * matrix[2] * matrix[15] -
+      matrix[4] * matrix[3] * matrix[14] -
+      matrix[12] * matrix[2] * matrix[7] +
+      matrix[12] * matrix[3] * matrix[6];
+
+    inv[10] = matrix[0] * matrix[5] * matrix[15] -
+      matrix[0] * matrix[7] * matrix[13] -
+      matrix[4] * matrix[1] * matrix[15] +
+      matrix[4] * matrix[3] * matrix[13] +
+      matrix[12] * matrix[1] * matrix[7] -
+      matrix[12] * matrix[3] * matrix[5];
+
+    inv[14] = -matrix[0] * matrix[5] * matrix[14] +
+      matrix[0] * matrix[6] * matrix[13] +
+      matrix[4] * matrix[1] * matrix[14] -
+      matrix[4] * matrix[2] * matrix[13] -
+      matrix[12] * matrix[1] * matrix[6] +
+      matrix[12] * matrix[2] * matrix[5];
+
+    inv[3] = -matrix[1] * matrix[6] * matrix[11] +
+      matrix[1] * matrix[7] * matrix[10] +
+      matrix[5] * matrix[2] * matrix[11] -
+      matrix[5] * matrix[3] * matrix[10] -
+      matrix[9] * matrix[2] * matrix[7] +
+      matrix[9] * matrix[3] * matrix[6];
+
+    inv[7] = matrix[0] * matrix[6] * matrix[11] -
+      matrix[0] * matrix[7] * matrix[10] -
+      matrix[4] * matrix[2] * matrix[11] +
+      matrix[4] * matrix[3] * matrix[10] +
+      matrix[8] * matrix[2] * matrix[7] -
+      matrix[8] * matrix[3] * matrix[6];
+
+    inv[11] = -matrix[0] * matrix[5] * matrix[11] +
+      matrix[0] * matrix[7] * matrix[9] +
+      matrix[4] * matrix[1] * matrix[11] -
+      matrix[4] * matrix[3] * matrix[9] -
+      matrix[8] * matrix[1] * matrix[7] +
+      matrix[8] * matrix[3] * matrix[5];
+
+    inv[15] = matrix[0] * matrix[5] * matrix[10] -
+      matrix[0] * matrix[6] * matrix[9] -
+      matrix[4] * matrix[1] * matrix[10] +
+      matrix[4] * matrix[2] * matrix[9] +
+      matrix[8] * matrix[1] * matrix[6] -
+      matrix[8] * matrix[2] * matrix[5];
+
+    det = 1.0f / det;
+
+    for (unsigned int i = 0; i < 16; ++i)
+      matrix[i] = inv[i] * det;
+  }
+
   void operator =(const Mat4& other) 
   {
     matrix[0] = other.matrix[0];  matrix[1] = other.matrix[1];  matrix[2] = other.matrix[2];  matrix[3] = other.matrix[3];
@@ -272,5 +405,3 @@ struct Mat4
   }
 };
 // ========================================================
-
-#endif // __MATH_H__
